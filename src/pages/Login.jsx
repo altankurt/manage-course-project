54ed556b7b6eca33be42822beb75781b5d234f3c
Altan Kurt <aaltankurt@gmail.com>
2023-07-04 02:12:50 +0300

    feat: add logo and title to login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,22 @@ export default function Login() {
   return (
     <div className="relative w-full h-screen bg-gradient font-fontFamily">
       <div className="login-form flex justify-center items-center h-full">
-        <form className="card w-[475px] h-[550px] mx-auto rounded-2xl shadow bg-white p-8"></form>
+        <form className="card w-[475px] h-[550px] mx-auto rounded-2xl shadow bg-white p-8">
+          <div className="logo flex mb-[43px] justify-center items-center relative">
+            <svg
+              width="6"
+              height="39"
+              viewBox="0 0 6 39"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M3 0L3 39" stroke="#F8D442" strokeWidth="6" />
+            </svg>
+            <h1 className="manage-courses px-3 text-[32px] items-center font-bold text-black uppercase">
+              Manage Courses
+            </h1>
+          </div>
+        </form>
       </div>
     </div>
   );
