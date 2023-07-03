2c88976e8c85316eeb20ecfb636238ac6ce03bfa
Altan Kurt <aaltankurt@gmail.com>
2023-07-03 12:45:52 +0300

    feat: add login component with imports and add main structure and form section to login component

---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Login() {
+  return (
+    <div className="relative w-full h-screen bg-gradient font-fontFamily">
+      <div className="login-form flex justify-center items-center h-full">
+        <form className="card w-[475px] h-[550px] mx-auto rounded-2xl shadow bg-white p-8"></form>
+      </div>
+    </div>
+  );
+}
