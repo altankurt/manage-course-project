4155dc5358a7d1738fafcc5ad4a4eb2c46f3d108
Altan Kurt <aaltankurt@gmail.com>
2023-07-03 11:36:00 +0300

    feat: define font family and colors, remove unnecessary files from project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      fontFamily: ['Montserrat', 'sans-serif'],
+    },
+
+    extend: {
+      colors: {
+        customYellowDark: '#feaf00',
+        customYellowLight: '#f8d442',
+      },
+    },
   },
   plugins: [],
 };
