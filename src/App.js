08e6f65d3301beb856fee96e71d66c994c4fc8e7
Altan Kurt <aaltankurt@gmail.com>
2023-07-04 23:07:54 +0300

    refactor: update routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
 
