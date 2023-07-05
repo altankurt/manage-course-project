0768530c834ff12a7c606bd8228de0219260a07d
Altan Kurt <aaltankurt@gmail.com>
2023-07-05 12:45:19 +0300

    refactor: restructure root component and import sidebar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route
+        path="/dashboard/*"
+        element={
+          <>
+            <Sidebar />
+            <Dashboard />
+          </>
+        }
+      />
     </Routes>
   );
 }
