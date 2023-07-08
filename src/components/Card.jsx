04bb881a7fe8717b2370e6b7f4c3131aa1734e28
Altan Kurt <aaltankurt@gmail.com>
2023-07-08 17:10:25 +0300

    fix: replace svg tag with div tag in card component to prevent overflow issue

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ const Card = ({ icon, title, value, backgroundColor }) => {
       <div
         className={`w-[255px] h-[157px] m-[30px] rounded-lg ${backgroundColor}`}
       >
-        <svg className="pl-5 pt-5">{icon}</svg>
+        <div className="pl-5 pt-5">{icon}</div>
         <h2 className="pl-5 text-neutral-500 text-sm font-medium">{title}</h2>
         <p className="pr-5 pb-5 flex justify-end text-black text-[30px] font-bold">
           {value}
