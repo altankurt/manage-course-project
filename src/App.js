e604d5ac2aaba8a459aa37d8a0e0bcaa9fa19667
Altan Kurt <aaltankurt@gmail.com>
2023-07-08 16:25:25 +0300

    refactor: rearrange root structure in app.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
-import Sidebar from './components/Sidebar';
+import Students from './pages/Students';
+import DashboardLayout from './components/Layouts/Dashboard';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route
-        path="/dashboard/*"
-        element={
-          <>
-            <Sidebar />
-            <Dashboard />
-          </>
-        }
-      />
+      <Route element={<DashboardLayout />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard/students" element={<Students />} />
+      </Route>
     </Routes>
   );
 }
